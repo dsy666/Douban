bdff54feacd1fadf5e17b008556b7eb4f32f686c
xcl2simple <1518227675@qq.com>
2019-11-21 18:48:35 +0800

    规范化

--- src/main/java/com/example/Douban/controller/FilmController.java
+++ src/main/java/com/example/Douban/controller/FilmController.java
@@ -7,23 +7,37 @@ import com.example.Douban.service.FilmEssayService;
 import com.example.Douban.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+
+/**
+ * Description: 电影Controller
+ *
+ * @author Mr.Du
+ * @version 1.0
+ * @date 2019/11/21 17:14
+ * @since JDK 1.8
+ */
+
 @RestController
 @CrossOrigin
 public class FilmController {
+
     @Autowired
     FilmService filmService=new FilmService();
+
     @Autowired
     FilmDetailService filmDetailService=new FilmDetailService();
+
     @Autowired
     FilmEssayService filmEssayService=new FilmEssayService();
-    @RequestMapping("/film")
+
+    @GetMapping("/film")
     public List<Film> filmShow() throws ParseException {
         Film f=new Film();
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
@@ -32,13 +46,15 @@ public class FilmController {
         filmslist=filmService.selectFromTimeAndArea(d, 13,50);
         return filmslist;
     }
-    @RequestMapping("/filmDetail")
+
+    @GetMapping("/filmDetail")
     public List<FilmDetail> filmDetailshow() throws ParseException {
         List<FilmDetail> filmDetailList;
         filmDetailList=filmDetailService.selectMessageById("10440138",10);
         return filmDetailList;
     }
-    @RequestMapping("/filmEssay")
+
+    @GetMapping("/filmEssay")
     public List<FilmEssay> filmEssayshow() throws ParseException{
         List<FilmEssay> filmEssayList;
         filmEssayList=filmEssayService.selectfilmEssay(10006301);
